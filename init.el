6a43ec2e0981b0d08748ddef9e7e1d064ea1cd8d
haiming Gong <364276231@qq.com>
2019-09-05 19:16:30 +0800

    add racer

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (rust-mode))))
+ '(package-selected-packages (quote (racer rust-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -18,5 +18,8 @@
 
 
 (add-to-list 'load-path "~/.emacs.d/elpa/rust-mode-20190517.2037/")
+(add-to-list 'load-path "~/.emacs.d/elpa/racer-20190610.800/")
 (autoload 'rust-mode "rust-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+
+(require 'racer)
