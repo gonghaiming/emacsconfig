f6e1a30bb8a58c568782bdcf984ccaf97728f4ff
haiming Gong <364276231@qq.com>
2019-09-24 04:03:55 +0800

    add rust config

--- init.el
+++ init.el
@@ -1,6 +1,11 @@
+
+
+
+
+
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
+(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 (package-initialize)
 
 (custom-set-variables
@@ -8,7 +13,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (racer rust-mode))))
+ '(custom-safe-themes
+   (quote
+    ("11e57648ab04915568e558b77541d0e94e69d09c9c54c06075938b6abc0189d8" default)))
+ '(package-selected-packages
+   (quote
+    (company-tabnine molokai-theme color-theme company racer rust-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -17,9 +27,21 @@
  )
 
 
-(add-to-list 'load-path "~/.emacs.d/elpa/rust-mode-20190517.2037/")
-(add-to-list 'load-path "~/.emacs.d/elpa/racer-20190610.800/")
-(autoload 'rust-mode "rust-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+;;(add-to-list 'load-path "~/.emacs.d/elpa/rust-mode-20190517.2037/")
+;;(add-to-list 'load-path "~/.emacs.d/elpa/racer-20190610.800/")
+;;(autoload 'rust-mode "rust-mode" nil t)
+;;(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
 
 (require 'racer)
+(require 'company)
+
+(add-hook 'prog-mode-hook 'company-mode)
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(load-theme 'molokai t)
+
+(require 'company-tabnine)
+(add-to-list 'company-backends #'company-tabnine)
+
+(setq company-idle-delay 0)
+(setq company-show-numbers t)
